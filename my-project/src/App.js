fd526858a1a9cff44efbec11920ebdd1252a0b2b
liu zhengdong <735083049@qq.com>
2019-09-28 01:36:05 +0800

    no

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import Button from 'antd/es/button';
+import {Row,Col,Card} from 'antd';
+//import Button from 'antd/es/button';
 import './App.css';
 
 
@@ -7,9 +8,46 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Button type="primary">Button</Button>
-      </div>
+      <div className="gutter-example">
+    <Row gutter={16}>
+      <Col className="gutter-row" span={8}>
+        <div className="gutter-box">col-8</div>
+      </Col>
+      <Col className="gutter-row" span={8}>
+        <div className="gutter-box">col-8</div>
+      </Col>
+      <Col className="gutter-row" span={8}>
+        <div className="gutter-box">col-8</div>
+      </Col>
+    </Row>
+    <div className="card-style" style={{ background: '#ECECEC', padding: '30px',display:'flex'}}>
+    <Card title="Card title" bordered={false} style={{ width: 300 , margin:5 }}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
+    <Card title="Card title" bordered={false} style={{ width: 300 , margin:5}}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
+    <Card title="Card title" bordered={false} style={{ width: 300 , margin:5}}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
+    <Card title="Card title" bordered={false} style={{ width: 300 , margin:5}}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
+    <Card title="Card title" bordered={false} style={{ width: 300 , margin:5}}>
+      <p>Card content</p>
+      <p>Card content</p>
+      <p>Card content</p>
+    </Card>
+  </div>
+  </div>
     );
   }
 }

88cdaca38bd0b89ba86e319de6cae80a7bbf84ed
liuser-macpro12 <735083049@qq.com>
2019-09-28 12:05:22 +0800

    ui

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import {Row,Col,Card} from 'antd';
+import { Card,Col,Row } from 'antd';
 //import Button from 'antd/es/button';
 import './App.css';
 
@@ -9,46 +9,26 @@ class App extends Component {
   render() {
     return (
       <div className="gutter-example">
-    <Row gutter={16}>
-      <Col className="gutter-row" span={8}>
-        <div className="gutter-box">col-8</div>
-      </Col>
-      <Col className="gutter-row" span={8}>
-        <div className="gutter-box">col-8</div>
-      </Col>
-      <Col className="gutter-row" span={8}>
-        <div className="gutter-box">col-8</div>
-      </Col>
-    </Row>
-    <div className="card-style" style={{ background: '#ECECEC', padding: '30px',display:'flex'}}>
-    <Card title="Card title" bordered={false} style={{ width: 300 , margin:5 }}>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
-    <Card title="Card title" bordered={false} style={{ width: 300 , margin:5}}>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
-    <Card title="Card title" bordered={false} style={{ width: 300 , margin:5}}>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
-    <Card title="Card title" bordered={false} style={{ width: 300 , margin:5}}>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
-    <Card title="Card title" bordered={false} style={{ width: 300 , margin:5}}>
-      <p>Card content</p>
-      <p>Card content</p>
-      <p>Card content</p>
-    </Card>
-    <div></div>
-  </div>
-  123
+        <div className="card-style" style={{ background: '#ECECEC', padding: '30px', display: 'flex' }}>
+          <Card title="Card title" bordered={false} style={{ width: 300, margin: 5 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+          <Card title="Card title" bordered={false} style={{ width: 300, margin: 5 }}>
+            <Row style={{width:300}}>
+              <Col span={7}>Ctrl+C</Col>
+              <Col span={14}>复制</Col>
+              <Col span={7}>Ctrl+v</Col>
+              <Col span={14}>粘贴</Col>
+            </Row>
+          </Card>
+          <Card title="Card title" bordered={false} style={{ width: 300, margin: 5 }}>
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+          </Card>
+        </div>
   </div>
     );
   }
